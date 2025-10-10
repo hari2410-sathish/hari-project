558354aa6b5d96149dde4a7cb49168f1407e4f96
hari2410-sathish <harisathish0698@gmail.com>
2025-10-10 02:51:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask in a CI/CD pipeline created successfully!"
+    return "Hello from Flask in a CI/CD pipeline created successfully then!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
